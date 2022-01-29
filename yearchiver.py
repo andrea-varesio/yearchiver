1c9735162e3e0c7cbcf0ebb2b6592603e3022e95
andrea-varesio <email@andreavaresio.com>
2022-01-29 20:40:14 +0100

    lint

--- yearchiver.py
+++ yearchiver.py
@@ -1,15 +1,14 @@
 #!/bin/python3
 #https://github.com/andrea-varesio/yearchiver
 
-from argparse import ArgumentParser
-from datetime import datetime
-from os import mkdir, stat, walk
-from os.path import basename, dirname, isdir, isfile, join, splitext
-from shutil import move
-from sys import exit
+import sys
+import argparse
+import datetime
+import os
+import os.path
+import shutil
 
-from inquirer import Checkbox, prompt
-from inquirer.themes import GreenPassion
+import inquirer
 
 def show_license():
     print('\n**************************************************')
@@ -20,93 +19,121 @@ def show_license():
     print('Full license available at https://github.com/andrea-varesio/yearchiver')
     print('**************************************************\n\n')
 
-def parser():
+def parse_arguments():
     show_license()
-    parser = ArgumentParser()
+    parser = argparse.ArgumentParser()
     extensions = parser.add_mutually_exclusive_group()
-    parser.add_argument('-i', '--input', help='Specify input directory', type=str)
-    parser.add_argument('-o', '--output', help='Specify output directory', type=str)
-    parser.add_argument('-q', '--quiet', help='Disable prompts and verbosity', action='store_true')
-    extensions.add_argument('-a', '--all', help='Archive all files, regardless of extension', action='store_true')
-    extensions.add_argument('-m', '--media', help='Archive files matching media extensions (default)', action='store_true')
-    extensions.add_argument('-f', '--filter', help='Only archive files matching this extension (ie .png)', type=str)
-    extensions.add_argument('--advanced-filter', help='Select file extensions to process through a prompt', action='store_true')
-    parser.add_argument('--dry-run', help='Visually display what files would be moved, without making any changes', action='store_true')
+    parser.add_argument('-i', '--input', type=str,
+                        help='Specify input directory')
+    parser.add_argument('-o', '--output', type=str,
+                        help='Specify output directory')
+    parser.add_argument('-q', '--quiet', action='store_true',
+                        help='Disable prompts and verbosity')
+    extensions.add_argument('-a', '--all', action='store_true',
+                            help='Archive all files, regardless of extension')
+    extensions.add_argument('-m', '--media', action='store_true',
+                            help='Archive files matching media extensions (default)')
+    extensions.add_argument('-f', '--filter', type=str,
+                            help='Only archive files matching this extension (ie .png)')
+    extensions.add_argument('--advanced-filter', action='store_true',
+                            help='Select file extensions to process through a prompt')
+    parser.add_argument('--dry-run', action='store_true',
+                        help='Display what files would be moved, without making any changes')
     return parser.parse_args()
 
 def filter_media():
-    filter_media = ['.asf', '.avi', '.bmp', '.bpg', '.cdr', '.crw', '.dng', '.dpx', '.dsc', '.dv', '.flv', '.gif', '.heic', '.icns', '.ico', '.jpg', '.jpeg', '.m2ts', '.mkv', '.mov', '.mp3', '.mp4', '.mpg', '.mpeg', '.mrw', '.oci', '.orf', '.pct', '.pcx', '.png', '.psb', '.psd','.psp', '.r3d', '.raf', '.raw', '.rdc', '.riff', '.wav', '.sit', '.skd', '.skp', '.spe', '.tib', '.rif', '.wdp', '.xcf', '.xv']
-    return filter_media
+    filter_media_ext = [
+        '.asf', '.avi', '.bmp', '.bpg', '.cdr', '.crw', '.dng', '.dpx', '.dsc',
+        '.dv', '.flv', '.gif', '.heic', '.icns', '.ico', '.jpg', '.jpeg',
+        '.m2ts', '.mkv', '.mov', '.mp3', '.mp4', '.mpg', '.mpeg', '.mpo',
+        '.mrw', '.oci', '.orf', '.pct', '.pcx', '.png', '.psb', '.psd','.psp',
+        '.r3d', '.raf', '.raw', '.rdc', '.riff', '.wav', '.sit', '.skd',
+        '.skp','.spe', '.tib', '.rif', '.wdp', '.xcf', '.xv'
+    ]
+    return filter_media_ext
 
 def filter_inquirer():
-    filter_inquirer = [
-        Checkbox('filter',
-            message = 'Select [space] the file extensions you want to process, then confirm [enter]',
-            choices = filter_media(),
-            default = filter_media()
-        ),
-    ]
-    return list(prompt(filter_inquirer, theme=GreenPassion()).values())[0]
+    filter_inquirer_param = [inquirer.Checkbox(
+        'filter',
+        message = '[space] to select, [enter] to confirm',
+        choices = filter_media(),
+        default = filter_media()
+    )]
+    return list(inquirer.prompt(
+        filter_inquirer_param,
+        theme=inquirer.themes.GreenPassion()
+    ).values())[0]
 
-def get_year():
-    created_timestamp = datetime.fromtimestamp(min([stat(join(subdir, file)).st_ctime, stat(join(subdir, file)).st_mtime]))
+def get_year(subdir, file, output_dir):
+    created_timestamp = datetime.datetime.fromtimestamp(min([
+        os.stat(os.path.join(subdir, file)).st_ctime,
+        os.stat(os.path.join(subdir, file)).st_mtime
+    ]))
     created_year = created_timestamp.strftime('%Y')
-    output_dir_year = join(output_dir, str(created_year))
+    output_dir_year = os.path.join(output_dir, str(created_year))
     return output_dir_year
 
-def loop():
-    if args.dry_run and not isfile(join(output_dir_year, file)):
-        print(f'{join(subdir, file)} > {output_dir_year}')
+def loop(args, subdir, file, output_dir_year):
+    if args.dry_run and not os.path.isfile(os.path.join(output_dir_year, file)):
+        print(f'{os.path.join(subdir, file)} > {output_dir_year}')
     elif not args.dry_run:
-        archive()
+        archive(subdir, file, output_dir_year)
 
-def archive():
-    if not isdir(output_dir_year):
-        mkdir(output_dir_year)
-    if not isfile(join(output_dir_year, file)):
-        move(join(subdir, file), output_dir_year)
+def archive(subdir, file, output_dir_year):
+    if not os.path.isdir(output_dir_year):
+        os.mkdir(output_dir_year)
+    if not os.path.isfile(os.path.join(output_dir_year, file)):
+        shutil.move(os.path.join(subdir, file), output_dir_year)
 
-args = parser()
+def process_arguments():
+    args = parse_arguments()
 
-if args.input and isdir(args.input):
-    input_dir = args.input
-elif args.input:
-    print(f'{args.input} is not a valid path')
-    exit(1)
-elif not args.input:
-    input_dir = dirname(__file__)
+    if args.input and os.path.isdir(args.input):
+        input_dir = args.input
+    elif args.input:
+        print(f'{args.input} is not a valid path')
+        sys.exit(1)
+    elif not args.input:
+        input_dir = os.path.dirname(__file__)
 
-if args.output and isdir(args.output):
-    output_dir = args.output
-elif args.output:
-    print(f'{args.output} is not a valid path')
-    exit(1)
-elif not args.output:
-    output_dir = dirname(__file__)
+    if args.output and os.path.isdir(args.output):
+        output_dir = args.output
+    elif args.output:
+        print(f'{args.output} is not a valid path')
+        sys.exit(1)
+    elif not args.output:
+        output_dir = os.path.dirname(__file__)
 
-if args.filter and args.filter.startswith('.'):
-    filter = [args.filter]
-elif args.filter:
-    filter = [f'.{args.filter}']
-elif args.advanced_filter:
-    filter = filter_inquirer()
-else:
-    filter = filter_media()
+    if args.filter and args.filter.startswith('.'):
+        ext_filter = [args.filter]
+    elif args.filter:
+        ext_filter = [f'.{args.filter}']
+    elif args.advanced_filter:
+        ext_filter = filter_inquirer()
+    else:
+        ext_filter = filter_media()
 
-if not args.quiet:
-    show_license()
+    if not args.quiet and not args.input and not args.output and not args.dry_run:
+        print('Process files in current working directory?')
+        print(os.path.dirname(__file__))
+        if input('Proceed (P) or abort (A)? ').lower() != 'p':
+            print('Okay, exiting...')
+            sys.exit(0)
 
-if not args.quiet and not args.input and not args.output and not args.dry_run:
-    print('Process files in current working directory?')
-    print(dirname(__file__))
-    if input('Proceed (P) or abort (A)? ').lower() != 'p':
-        print('Okay, exiting...')
-        exit(0)
+    return args, input_dir, output_dir, ext_filter
 
-for subdir, dirs, files in walk(input_dir):
-    for file in files:
-        output_dir_year = get_year()
-        if file != basename(__file__) and args.all:
-            loop()
-        elif file != basename(__file__) and splitext(file)[1].lower() in filter:
-            loop()
+def main():
+    args, input_dir, output_dir, ext_filter = process_arguments()
+    for subdir, dirs, files in os.walk(input_dir):
+        for file in files:
+            output_dir_year = get_year(subdir, file, output_dir)
+            if file != os.path.basename(__file__) and args.all:
+                loop(args, subdir, file, output_dir_year)
+            elif (
+                file != os.path.basename(__file__) and
+                os.path.splitext(file)[1].lower() in ext_filter
+            ):
+                loop(args, subdir, file, output_dir_year)
+
+if __name__ == "__main__":
+    main()
